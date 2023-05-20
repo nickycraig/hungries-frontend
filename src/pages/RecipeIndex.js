5b8dbb92c3811a207deaaced9033ac93b1cb4ef8
nickycraig <nickycraig@gmail.com>
2023-05-20 10:41:43 -0700

    working on new recipe form

--- src/pages/RecipeIndex.js
+++ src/pages/RecipeIndex.js
@@ -22,78 +22,48 @@ function RecipeIndex () {
     function loaded() {
         return(
             <>
-            <h2>Soups</h2>
                 {recipes.map((recipe, idx) => {
-                    if (recipes[idx].type === "soup") {
                         return(
                             <div key={idx}>
-                                <h3>{recipes[idx].name}</h3>
+                                <h3>{recipe.name}</h3>
                             </div>
                         )
-                    }
                 })}
-            
-            <h2>Salads</h2>
-                {recipes.map((recipe, idx) => {
-                    if (recipes[idx].type === "salad") {
-                        return(
-                            <div key={idx}>
-                                <h3>{recipes[idx].name}</h3>
-                            </div>
-                        )
-                    }
-                })}
-            
-            <h2>Appetizers</h2>
-                {recipes.map((recipe, idx) => {
-                    if (recipes[idx].type === "appetizer") {
-                        return(
-                            <div key={idx}>
-                                <h3>{recipes[idx].name}</h3>
-                            </div>
-                        )
-                    }
-                })}
-
-            <h2>Entrees</h2>
-                {recipes.map((recipe, idx) => {
-                    if (recipes[idx].type === "entree") {
-                        return(
-                            <div key={idx}>
-                                <h3>{recipes[idx].name}</h3>
-                            </div>
-                        )
-                    }
-                })}
-
-            <h2>Side Dishes</h2>
-                {recipes.map((recipe, idx) => {
-                    if (recipes[idx].type === "side dish") {
-                        return(
-                            <div key={idx}>
-                                <h3>{recipes[idx].name}</h3>
-                            </div>
-                        )
-                    }
-                })}
-
-            <h2>Desserts</h2>
-                {recipes.map((recipe, idx) => {
-                    if (recipes[idx].type === "dessert") {
-                        return(
-                            <div key={idx}>
-                                <h3>{recipes[idx].name}</h3>
-                            </div>
-                        )
-                    }
-                })}
+           
             </>
         )
     }
 
     return(
         <>
-            {recipes ? loaded() : <h2>Loading...</h2>}
+            <form>
+                <label for="name">Recipe </label>
+                <input type="text" name="recipe" id="name" placeholder="Name of recipe"/>
+                <br/>
+                <label for="type">Type </label>
+                <select name="type" id="type">
+                    <option value="soup">Soup</option>
+                    <option value="salad">Salad</option>
+                    <option value="entree">Entree</option>
+                    <option value="side dish">Side Dish</option>
+                    <option value="appetizer">Appetizer</option>
+                    <option value="dessert">Dessert</option>
+                </select>
+                <br/>
+                <label for="image">Image </label>
+                <input type="text" name="image" id="image" placeholder="upload image URL"/>
+                <br/>
+                <label for="cuisine">Cuisine </label>
+                <input type="text" name="cuisine" id="cuisine" placeholder="chinese, vegetarian, etc"/>
+                <br/>
+                <label for="comments">Comments </label>
+                <input type="text" name="comments" id="comments" placeholder="recipe notes/changes/suggestions"/>
+                <br/>
+                <label for="recipeLink">Recipe Link </label>
+                <input type="text" name="recipeLink" id="recipeLink" placeholder="chinese, vegetarian, etc"/>
+                <br/>
+            </form>
+            {recipes.length ? loaded(recipes) : <h2>Loading...</h2>}
         </>
     )
 }
