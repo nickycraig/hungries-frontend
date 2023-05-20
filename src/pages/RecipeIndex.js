a93caf51fbe879981f95224e094eb4fb606088a9
nickycraig <nickycraig@gmail.com>
2023-05-20 13:35:25 -0700

    adds on recipe show page

--- src/pages/RecipeIndex.js
+++ src/pages/RecipeIndex.js
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function RecipeIndex () {
     const [recipes, setRecipes] = useState([]);
+    const [recipeForm, setRecipeForm] = useState({
+        name: "",
+        type: "",
+        image: "",
+        cuisine: [],
+        comments: "",
+        recipeLink: ""
+
+    })
 
     async function getRecipes() {
         try {
@@ -25,7 +35,9 @@ function RecipeIndex () {
                 {recipes.map((recipe, idx) => {
                         return(
                             <div key={idx}>
-                                <h3>{recipe.name}</h3>
+                                <Link to={`/recipes/${recipe._id}`}>
+                                    <h3>{recipe.name}</h3>
+                                </Link>
                             </div>
                         )
                 })}
@@ -34,14 +46,39 @@ function RecipeIndex () {
         )
     }
 
+    function handleChange(e) {
+        console.log(e.target);
+        setRecipeForm((previousFormState) => ({
+            ...previousFormState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    // console.log(recipeForm);
+    async function handleSubmit(e) {
+        try {
+            e.preventDefault();
+            await fetch('http://localhost:3000/recipes', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(recipeForm)
+            })
+            getRecipes();
+            e.target.reset();
+        } catch(err) {
+            console.log(err);
+        }
+    } 
+
     return(
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label for="name">Recipe </label>
-                <input type="text" name="recipe" id="name" placeholder="Name of recipe"/>
+                <input type="text" name="name" id="name" onChange={handleChange} placeholder="Name of recipe"/>
                 <br/>
                 <label for="type">Type </label>
-                <select name="type" id="type">
+                <select name="type" id="type" onChange={handleChange}>
                     <option value="soup">Soup</option>
                     <option value="salad">Salad</option>
                     <option value="entree">Entree</option>
@@ -51,17 +88,18 @@ function RecipeIndex () {
                 </select>
                 <br/>
                 <label for="image">Image </label>
-                <input type="text" name="image" id="image" placeholder="upload image URL"/>
+                <input type="text" name="image" id="image" onChange={handleChange} placeholder="upload image URL"/>
                 <br/>
                 <label for="cuisine">Cuisine </label>
-                <input type="text" name="cuisine" id="cuisine" placeholder="chinese, vegetarian, etc"/>
+                <input type="text" name="cuisine" id="cuisine" onChange={handleChange} placeholder="chinese, vegetarian, etc"/>
                 <br/>
                 <label for="comments">Comments </label>
-                <input type="text" name="comments" id="comments" placeholder="recipe notes/changes/suggestions"/>
+                <input type="text" name="comments" id="comments" onChange={handleChange} placeholder="recipe notes/changes/suggestions"/>
                 <br/>
                 <label for="recipeLink">Recipe Link </label>
-                <input type="text" name="recipeLink" id="recipeLink" placeholder="chinese, vegetarian, etc"/>
+                <input type="text" name="recipeLink" id="recipeLink" onChange={handleChange} placeholder="paste recipe URL"/>
                 <br/>
+                <button>Add new recipe</button>
             </form>
             {recipes.length ? loaded(recipes) : <h2>Loading...</h2>}
         </>
--- src/pages/RecipeShow.js
+++ src/pages/RecipeShow.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 function RecipeShow () {
+    const [ recipe, setRecipe ] = useState(null);
+    const { recipeId } = useParams();
+    console.log(recipeId);
+    async function getRecipe() {
+        try {
+            let myRecipe = await fetch(`http://localhost:3000/recipes/${recipeId}`);
+            myRecipe = await myRecipe.json();
+            setRecipe(myRecipe);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+    console.log(recipe);
+
+    function recipeLoaded() {
+        return(
+            <>
+                <h2>{recipe.name}</h2>
+                <h4>({recipe.cuisine.join(', ')})</h4>
+                <img src={recipe.image} alt='recipe photo' />
+                <p>Comments: {recipe.comments}</p>
+                <a href={recipe.recipeLink}>Link to full recipe</a>
+            </>
+        )
+    }
+
+    useEffect(() => {
+        getRecipe();
+    }, []);
+
     return(
-        <h2>This is my recipe show page</h2>
+        <>
+            {recipe ? recipeLoaded() : <h2>Loading...</h2>}
+        </>
     )
+
 }
+
 
 export default RecipeShow;
