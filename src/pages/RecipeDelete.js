7012b85702f24878d42c4cff265274fb21828fbd
nickycraig <nickycraig@gmail.com>
2023-05-20 14:18:25 -0700

    delete page is working

--- src/pages/RecipeDelete.js
+++ src/pages/RecipeDelete.js
@@ -1,9 +1,30 @@
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
 function RecipeDelete() {
+    const {recipeId} = useParams();
+
+    async function deleteMyRecipe() {
+        try {
+            await fetch(`http://localhost:3000/recipes/${recipeId}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application.json"
+                }
+            })
+        } catch(err) {
+            console.log(err);
+        }
+    }
     return(
         <>
             <h2>Are you sure your Hungry Guys won't eat this?</h2>
-            <button>Yes, they hated it!</button>
-            <button>No, let's try it again!</button>
+            <Link to='/recipes'>
+                <button onClick={deleteMyRecipe}>Yes, they hated it!</button>
+            </Link>
+            <Link to={`/recipes/${recipeId}`}>
+                <button>No, let's try it again!</button>
+            </Link>
         </>
     )
 }
