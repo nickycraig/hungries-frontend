e9ea170c11d5a05d08ee0a0b93e96ffcdb8e0a66
nickycraig <nickycraig@gmail.com>
2023-05-28 10:49:09 -0700

    added form styling on recipe index page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 function Header () {
     return(
         <>
-            <nav>
+            <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
                 <Link to='/'>Home</Link>
+            <h1 className='text-4xl'>Hungry Guys Recipe File</h1>
                 <Link to='/recipes'>Recipes</Link>
             </nav>
-            <h1>Hungry Guys Recipe File</h1>
         </>
     )
 }
--- src/pages/RecipeIndex.js
+++ src/pages/RecipeIndex.js
@@ -33,7 +33,7 @@ function RecipeIndex () {
     function loaded() {
         return(
             <>
-            <h2>Soups</h2>
+            <h2 className='text-2xl'>Soups</h2>
                 {recipes.filter(recipe => recipe.type === "soup").map((recipe, idx) => {
                     return(
                         <div key={idx}>
@@ -44,7 +44,7 @@ function RecipeIndex () {
                     )
                 } )}
            
-            <h2>Salads</h2>
+            <h2 className='text-2xl'>Salads</h2>
                 {recipes.filter(recipe => recipe.type === "salad").map((recipe, idx) => {
                     return(
                         <div key={idx}>
@@ -55,7 +55,7 @@ function RecipeIndex () {
                     )
                 } )}
 
-            <h2>Entrees</h2>
+            <h2 className='text-2xl'>Entrees</h2>
                 {recipes.filter(recipe => recipe.type === "entree").map((recipe, idx) => {
                     return(
                         <div key={idx}>
@@ -66,7 +66,7 @@ function RecipeIndex () {
                     )
                 } )}
 
-            <h2>Side Dishes</h2>
+            <h2 className='text-2xl'>Side Dishes</h2>
                 {recipes.filter(recipe => recipe.type === "side dish").map((recipe, idx) => {
                     return(
                         <div key={idx}>
@@ -77,7 +77,7 @@ function RecipeIndex () {
                     )
                 } )}
 
-            <h2>Appetizers</h2>
+            <h2 className='text-2xl'>Appetizers</h2>
                 {recipes.filter(recipe => recipe.type === "appetizer").map((recipe, idx) => {
                     return(
                         <div key={idx}>
@@ -88,7 +88,7 @@ function RecipeIndex () {
                     )
                 } )}
 
-            <h2>Desserts</h2>
+            <h2 className='text-2xl'>Desserts</h2>
                 {recipes.filter(recipe => recipe.type === "dessert").map((recipe, idx) => {
                     return(
                         <div key={idx}>
@@ -128,37 +128,133 @@ function RecipeIndex () {
     } 
 
     return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <label for="name">Recipe </label>
-                <input type="text" name="name" id="name" onChange={handleChange} placeholder="Name of recipe"/>
-                <br/>
-                <label for="type">Type </label>
-                <select name="type" id="type" onChange={handleChange}>
+        <section class="bg-gray-100">
+  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+      <div class="lg:col-span-2 lg:py-12">
+        <p class="max-w-xl text-lg">
+          Use the form to add a new recipe to feed your Hungry Guys. Be sure to include some notes in the comments section: what you liked, what you changed or modified, etc. Let's eat!
+        </p>
+      </div>
+
+      <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+        <form onSubmit={handleSubmit} action="" class="space-y-4">
+          <div>
+            <label class="sr-only" for="name">Recipe</label>
+            <input
+              class="w-full rounded-lg border-gray-200 p-3 text-sm"
+              placeholder="name of recipe"
+              type="text"
+              id="name"
+              onChange={handleChange} 
+            />
+          </div>
+
+          <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div>
+              <label class="sr-only" for="cuisine">Cuisine</label>
+              <input
+                class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="chinese, vegan, etc."
+                type="text"
+                id="cuisine"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <label class="sr-only" for="type">Type</label>
+              <select
+                class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="recipe type"
+                id="type"
+                onChange={handleChange}>
                     <option value="soup">Soup</option>
                     <option value="salad">Salad</option>
                     <option value="entree">Entree</option>
                     <option value="side dish">Side Dish</option>
                     <option value="appetizer">Appetizer</option>
                     <option value="dessert">Dessert</option>
-                </select>
-                <br/>
-                <label for="image">Image </label>
-                <input type="text" name="image" id="image" onChange={handleChange} placeholder="upload image URL"/>
-                <br/>
-                <label for="cuisine">Cuisine </label>
-                <input type="text" name="cuisine" id="cuisine" onChange={handleChange} placeholder="chinese, vegetarian, etc"/>
-                <br/>
-                <label for="comments">Comments </label>
-                <input type="text" name="comments" id="comments" onChange={handleChange} placeholder="recipe notes/changes/suggestions"/>
-                <br/>
-                <label for="recipeLink">Recipe Link </label>
-                <input type="text" name="recipeLink" id="recipeLink" onChange={handleChange} placeholder="paste recipe URL"/>
-                <br/>
-                <button>Add new recipe</button>
-            </form>
-            {recipes.length ? loaded(recipes) : <h2>Loading...</h2>}
-        </>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <label class="sr-only" for="comments">Comments</label>
+            <textarea
+              class="w-full rounded-lg border-gray-200 p-3 text-sm"
+              placeholder="add comments here"
+              rows="4"
+              id="comments"
+              onChange={handleChange}
+            ></textarea>
+          </div>
+
+          <div>
+            <label class="sr-only" for="image">Image</label>
+            <input
+              class="w-full rounded-lg border-gray-200 p-3 text-sm"
+              placeholder="add image URL"
+              type="text"
+              id="image"
+              onChange={handleChange} 
+            />
+          </div>
+          <div>
+            <label class="sr-only" for="recipeLink">Recipe Link</label>
+            <input
+              class="w-full rounded-lg border-gray-200 p-3 text-sm"
+              placeholder="add recipe URL"
+              type="text"
+              id="recipeLink"
+              onChange={handleChange} 
+            />
+          </div>
+          <div class="mt-4">
+            <button
+              type="submit"
+              class="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+            >
+              Add new recipe
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  {recipes.length ? loaded(recipes) : <h2>Loading...</h2>}
+</section>
+        // <section class="bg-gray-100"> 
+        //     <form onSubmit={handleSubmit}>
+        //         <label for="name">Recipe </label>
+        //         <input type="text" name="name" id="name" onChange={handleChange} placeholder="Name of recipe"/>
+        //         <br/>
+        //         <label for="type">Type </label>
+        //         <select name="type" id="type" onChange={handleChange}>
+        //             <option value="soup">Soup</option>
+        //             <option value="salad">Salad</option>
+        //             <option value="entree">Entree</option>
+        //             <option value="side dish">Side Dish</option>
+        //             <option value="appetizer">Appetizer</option>
+        //             <option value="dessert">Dessert</option>
+        //         </select>
+        //         <br/>
+        //         <label for="cuisine">Cuisine </label>
+        //         <input type="text" name="cuisine" id="cuisine" onChange={handleChange} placeholder="chinese, vegetarian, etc"/>
+        //         <br/>
+        //         <label for="comments">Comments </label>
+        //         <input type="text" name="comments" id="comments" onChange={handleChange} placeholder="recipe notes/changes/suggestions"/>
+        //         <br/>
+        //         <label for="image">Image </label>
+        //         <input type="text" name="image" id="image" onChange={handleChange} placeholder="upload image URL"/>
+        //         <br/>
+        //         <label for="recipeLink">Recipe Link </label>
+        //         <input type="text" name="recipeLink" id="recipeLink" onChange={handleChange} placeholder="paste recipe URL"/>
+        //         <br/>
+        //         <button>Add new recipe</button>
+        //     </form>
+        //     {recipes.length ? loaded(recipes) : <h2>Loading...</h2>}
+        // </section>
     )
 }
 
